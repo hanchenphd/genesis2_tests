61e86314dcb060e0146a312420be97ae4c59bd03
Tamar Sofer <tsofer@uw.edu>
2017-08-29 15:01:11 -0700

    updated comparison values
    comparing this package results to GENESIS, values are now required to
    be smaller than 1e-6.

--- tests/testthat/test_null_model_WLS_regression.R
+++ tests/testthat/test_null_model_WLS_regression.R
@@ -36,7 +36,7 @@ expect_true(all(nullmod$model.matrix == X))
 
 
 ## checks - GENESIS:
-expect_true(all(abs(nullmod$fixef - lmm.genesis$fixef) < 1e-5))
+expect_true(all(abs(nullmod$fixef - lmm.genesis$fixef) < 1e-6))
 expect_true(all(abs(nullmod$betaCov - lmm.genesis$betaCov) < 1e-6))
 expect_true(all(abs(nullmod$resid.marginal - lmm.genesis$resid.response) < 1e-8))
 expect_true(all(abs(nullmod$logLik - lmm.genesis$logLik) < 1e-6))
@@ -45,16 +45,15 @@ expect_true(all(abs(nullmod$logLikR == lmm.genesis$logLikR) < 1e-6))
 ## currently GENESIS has a mistake, in AUC calculation it uses the number of 
 ## matrices and groups used, but not the actual number of non-zero variance components. 
 ## so this "2" fixes for it. 
-expect_true(all(abs(nullmod$AIC - (lmm.genesis$AIC - 2)) < 1e-5))
+expect_true(all(abs(nullmod$AIC - (lmm.genesis$AIC - 2)) < 1e-6))
 expect_true(all(nullmod$workingY == lmm.genesis$workingY))
 expect_true(all(nullmod$model.matrix == lmm.genesis$model.matrix))
 expect_true(all(abs(nullmod$varComp - lmm.genesis$varComp[2:3]) < 1e-6))
-expect_true(all(abs(nullmod$varCompCov - lmm.genesis$varCompCov[2:3, 2:3]) < 1e-4))  ## smaller than 1e-4, but not 1e-5, need to check!
+expect_true(all(abs(nullmod$varCompCov - lmm.genesis$varCompCov[2:3, 2:3]) < 1e-6))  
 expect_equal(nullmod$family$family, lmm.genesis$family$family)
 expect_null(nullmod$zeroFLAG)
-expect_true(all(abs(nullmod$cholSigmaInv - lmm.genesis$cholSigmaInv) < 1e-5))
-expect_true(all(abs(nullmod$RSS - lmm.genesis$RSS) < 1e-5))
+expect_true(all(abs(nullmod$cholSigmaInv - lmm.genesis$cholSigmaInv) < 1e-6))
+expect_true(all(abs(nullmod$RSS - lmm.genesis$RSS) < 1e-6))
 
 
-## need to check more, why results are not <1e-6 ?
 })
