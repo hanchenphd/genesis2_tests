68d83e738e679e9768bb0a58cd02c281e31081f0
Tamar Sofer <tsofer@uw.edu>
2017-09-01 12:51:12 -0700

    Created a function that tests a variance component based on a null model
    (for testing heritability). it uses the likelihood ratio test.

---
+++ R/testSingleVarianceComponent.R
@@ -0,0 +1,28 @@
+
+
+## a function that tests a variance component (e.g. genetic relatedness, for testing heritability) 
+## using the likelihood ration test. 
+testSingleVarianceComponent <- function(nullmod, varCompName, covMatList, group.idx = NULL, AIREML.tol = 1e-6, maxIter = 100, dropZeros = TRUE, verbose = TRUE){
+	
+	ind.test <- match(varCompName, names(covMatList))
+	
+	if (length(covMatList) == 1){
+		nullmod.noVarComp <- fitNullModel(nullmod$outcome, X = nullmod$model.matrix, covMatList = NULL, 
+				group.idx = group.idx, family = nullmod$family$family, start = NULL,
+				AIREML.tol = AIREML.tol, maxIter= maxIter, dropZeros = dropZeros, verbose = verbose)
+
+	} else{
+		nullmod.noVarComp <- fitNullModel(nullmod$outcome, X = nullmod$model.matrix, covMatList = covMatList[-ind.test], 
+				group.idx = group.idx, family = nullmod$family$family, start = nullmod$varComp,
+				AIREML.tol = AIREML.tol, maxIter= maxIter, dropZeros = dropZeros, verbose = verbose)	
+		}
+	
+
+	
+	
+	test.stat <- -2*(nullmod.noVarComp$logLik - nullmod$logLik)			
+	
+	pval <- 0.5*pchisq(test.stat, df = length(nullmod$varComp), lower.tail = FALSE) + 0.5*pchisq(test.stat, df = length(nullmod$varComp) - 1, lower.tail = F)  
+	
+	return(pval)
+}
